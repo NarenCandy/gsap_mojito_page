f053d4e3164067c7b8b556b9148bb79e4f9ddbaa
NarenCandy <narencrazy007@gmail.com>
2025-06-28 17:22:26 +0530

    Implemented the first part of the Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,21 @@
 import React, { useEffect, useRef } from 'react'
 
 import { gsap } from 'gsap'
+import NavBar from './components/NavBar'
+import Hero from './components/Hero'
 
 
 
 
 const App = () => {
-  const textRef = useRef(null)
-  const handleCLick = () =>{
-    gsap.to(textRef.current, {
-      duration: 1,
-      x: 250,
-      
-      scale: 2,
-      ease: 'power1.inOut',
-    })
-  }
+  
   return (
-    <div ref={textRef}>App
     <div>
-      <button onClick={handleCLick}>Click me
-      </button>
+    <NavBar />
+    <Hero />
+    <div className='h-dvh bg-black'/>
     </div>
-    </div>
+    
   )
 }
 
