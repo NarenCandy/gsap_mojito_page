f094e078317eb6fb06bcbff6f8cdc812a10f622a
NarenCandy <narencrazy007@gmail.com>
2025-06-28 23:33:28 +0530

    update ui

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -46,7 +46,7 @@ const About = () => {
                     </p>
                     <div>
                         <p className='md:text-3xl text-xl font-bold'>
-                            <span>4.5</span>5
+                            <span>4.5/</span>5
 
                         </p>
                         <p className='text-sm text-white-100'>
